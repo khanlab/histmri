62246aae5e344d882c339fe58ddb782ef19ac947
akhanf <alik@robarts.ca>
2019-08-17 22:23:11 -0400

    removed default padding, enabled openslide

--- matlab/generateFieldFraction.m
+++ matlab/generateFieldFraction.m
@@ -2,7 +2,8 @@ function  generateFieldFraction(tif,res_microns,pad_microns)
 %subj,specimen,slice,stain)
 
 %converted tifs from svs trigger a type warning, this suppresses it..
-warning('off','all')
+%warning('off','all')
+init_openslide;
 
 %default uses 100um+5um pad -- Maged's 20um used 50um pad -- 
 
@@ -16,7 +17,7 @@ end
 
 if ~exist('pad_microns')
     %padding of each chunk, in microns -- default is ~approx radius of neuron
-   pad_microns=5;
+   pad_microns=0;
    %by default, use legacy naming (100um_FeatureMaps)
    out_name=sprintf('%dum',res_microns);
 else
@@ -31,30 +32,20 @@ end
 
 [path,name,ext]=fileparts(tif);
 
+
 split=strsplit(name,'_');
 
-%get stain type at end of name:
-s=name;
-
-subj=name(1:8);
 stain_type=split{end};
 strct=split{end-2}; %Hp or Neo
 
-
-%currently only HE and LUXFB supported
-%if (~ (strcmp(stain_type,'LUXFB') || strcmp(stain_type,'HE')))
-%    disp(sprintf('Unsupported stain_type: %s',stain_type));
-%end
-
-
-
-lores_png=sprintf('%s/../%dum_png/%s.png',path,res_microns,name);
-
-if ~exist(lores_png)
-  system(sprintf('echo %s >> Missing_%dum_png.txt',lores_png,res_microns))
-else
-lores=imread(lores_png);
+if (length(split) == 4)
+	subj=split{1};
+elseif (length(split) == 5)
+	subj=[split{1},'_', split{2}];
 end
+
+
+
 
 
 
@@ -69,21 +60,7 @@ outmap=sprintf('%s/%s.mat',outdir,name);
 
 
 
-
-%0.5um/pixel
-hist_res=0.5;
-
-scalefac=res_microns./hist_res;
-
-
-%imgSizes=mexAperioTiff(tif);
-imgSizes=getAperioImgSizes(tif);
-
-
-ds_size=ceil(imgSizes(1,:)./scalefac);
-Nx=ds_size(1);
-Ny=ds_size(2);
-
+[openslidePointer,hist_res, Nx,Ny] =  prepChunks_openslide (tif, res_microns);
 
 
 
@@ -94,7 +71,7 @@ featureVec=zeros(Nx,Ny,length(features));
 
 
 %padwidth should be radius of pyramidal neuron (say 10um/2 = 5um)
-padWidth=pad_microns./hist_res; %in pixels
+padWidth=floor(pad_microns./hist_res); %in pixels
 
 
 
@@ -102,12 +79,12 @@ for i=1:Nx
 %    for i=roix
  
 % i
-disp(sprintf('%2.1f percent complete',double(i-1)./double(Nx)*100));
+fprintf('%2.1f percent complete\n',double(i-1)./double(Nx)*100);
     for j=1:Ny
 %        for j=roiy
         
   %  j;
-        [img]=getHiresChunkAperio(tif,Nx,Ny,i,j,0,padWidth); 
+        [img]=getHiresChunkOpenslide(openslidePointer,Nx,Ny,i,j,0,padWidth); 
         
 
        
